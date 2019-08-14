4097043d05c4ab94eddcf5727aa8e9fdcc61ad8c
Emma Tong <eat2004@gmail.com>
2019-08-14 08:21:49 -0400

    Changed RobotMap ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -31,8 +31,7 @@ public class RobotMap {
 
   public static final int INTAKE_MOTOR_PORT=3;
   public static final int SHOOTER_MOTOR_PORT=4;
-  public static final int HATCH_MOTOR=5;
 
-  public static final int HATCH_SOLENOID_PORT1=6;
-  public static final int HATCH_SOLENOID_PORT2=7;
+  public static final int HATCH_SOLENOID_PORT1=5;
+  public static final int HATCH_SOLENOID_PORT2=6;
 }
