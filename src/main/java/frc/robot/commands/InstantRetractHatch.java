0dcc8a6dc3e2066fc54235c6068e4c71c839163e
Emma Tong <eat2004@gmail.com>
2019-08-20 10:07:48 -0400

    Changed instant to regular command
    Changed instant commands for the hatch to regular due to HatchSubsystem change to motor instead of double solenoid

--- src/main/java/frc/robot/commands/InstantRetractHatch.java
+++ src/main/java/frc/robot/commands/InstantRetractHatch.java
@@ -4,45 +4,23 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+/*
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
 
-public class RetractHatch extends Command {
+public class RetractHatch extends InstantCommand {
   public RetractHatch() {
+    super();
     // Use requires() here to declare subsystem dependencies
     requires(Robot.m_hatchSubsystem);
   }
 
-  // Called just before this Command runs the first time
+  // Called once when the command executes
   @Override
   protected void initialize() {
-  }
-
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
     Robot.m_hatchSubsystem.retractHatch();
   }
-
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
-
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-    Robot.m_hatchSubsystem.turnOffHatch();
-  }
-
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-    end();
-  }
 }
+*/
